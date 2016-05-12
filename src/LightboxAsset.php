87b498d62dca2f7e52d27d672eb42b8954c5a72c
tsanchev <tsanchev@gmail.com>
2016-05-12 12:09:48 +0300

    fix src directories

--- src/LightboxAsset.php
+++ src/LightboxAsset.php
@@ -6,17 +6,16 @@
  * @link      https://github.com/hiqdev/yii2-asset-lightbox2
  * @package   yii2-asset-lightbox2
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015, HiQDev (https://hiqdev.com/)
  */
 
-namespace hiqdev\assets\lightbox2;
+namespace tsanchev\assets\lightbox2;
 
 class LightboxAsset extends \yii\web\AssetBundle
 {
     /**
      * {@inheritdoc}
      */
-    public $sourcePath = '@bower/lightbox2';
+    public $sourcePath = '@bower/lightbox2/dist/';
 
     /**
      * {@inheritdoc}
